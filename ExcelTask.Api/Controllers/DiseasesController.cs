8b6d53198a072278b746d235890d86951e89290e
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2023-07-08 15:21:30 +0600

    disease controller in api

--- ExcelTask.Api/Controllers/DiseasesController.cs
+++ ExcelTask.Api/Controllers/DiseasesController.cs
@@ -1,4 +1,5 @@
-﻿using ExcelTask.Application.IServices;
+﻿using ExcelTask.Application.DTOs;
+using ExcelTask.Application.IServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ExcelTask.Api.Controllers;
@@ -19,5 +20,33 @@ public class DiseasesController : ControllerBase
     {
         var diseases = await _diseaseService.GetAllDiseasesAsync();
         return Ok(diseases);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(int id)
+    {
+        var disease = await _diseaseService.GetDiseaseByIdAsync(id);
+        return Ok(disease);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(DiseaseCreateDto disease)
+    {
+        await _diseaseService.AddDiseaseAsync(disease);
+        return Ok();
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Put(DiseaseUpdateDto disease)
+    {
+        await _diseaseService.UpdateDiease(disease);
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await _diseaseService.DeleteDiseaseAsync(id);
+        return Ok();
     }
 }
