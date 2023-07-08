e10416b0658611a75d194bbd208357c3908bc7e1
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2023-07-08 19:44:00 +0600

    disease index view

--- ExcelTask.Api/Controllers/DiseasesController.cs
+++ ExcelTask.Api/Controllers/DiseasesController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ExcelTask.Api.Controllers;
 
-[Route("api/[controller]")]
+[Route("api/[controller]/[action]")]
 [ApiController]
 public class DiseasesController : ControllerBase
 {
---
+++ ExcelTask.UI/Controllers/DiseasesController.cs
@@ -0,0 +1,34 @@
+﻿using ExcelTask.Application.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ExcelTask.UI.Controllers;
+
+public class DiseasesController : Controller
+{
+    Uri baseAddress = new Uri("https://localhost:7282/api");
+    private readonly HttpClient _client;
+
+    public DiseasesController()
+    {
+        _client = new HttpClient();
+        _client.BaseAddress = baseAddress;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        List<DiseaseViewDto> diseases = new List<DiseaseViewDto>();
+        HttpResponseMessage response = await _client.GetAsync(_client.BaseAddress + "/Diseases/Get");
+
+        if (response.IsSuccessStatusCode)
+        {
+            string data = await response.Content.ReadAsStringAsync();
+            diseases = JsonConvert.DeserializeObject<List<DiseaseViewDto>>(data);
+        }
+
+        return View(diseases);
+    }
+}
